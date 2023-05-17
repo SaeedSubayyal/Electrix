b35048a74db0dad1a63e4e5b530ab5b24062a8f3
SaeedSubayyal <127910222+SaeedSubayyal@users.noreply.github.com>
2023-05-17 23:34:38 +0500

    Delete Project(Admin + User) directory

--- Project(Admin + User)/electrix(ignore this folder these are your new code files)/schemas.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE admin (
-  admin_id INT AUTO_INCREMENT PRIMARY KEY,
-  admin_name VARCHAR(128) NOT NULL,
-  admin_email VARCHAR(255) UNIQUE NOT NULL,
-  admin_password VARCHAR(255) NOT NULL
-);
